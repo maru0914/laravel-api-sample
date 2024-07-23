072373593d1b26f6cff2d90799317f9acb496421
Maru <56859729+maru0914@users.noreply.github.com>
2024-07-23 16:01:17 +0900

    Customize 404 response

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +17,9 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->renderable(function (NotFoundHttpException $e, Request $request) {
+            if ($request->wantsJson() && !config('app.debug')) {
+                return response()->json(['message' => 'Object not found'], 404);
+            }
+        });
     })->create();
