623fae874d2557acda116d0ce5bc8bb06d0c4e1e
Maru <56859729+maru0914@users.noreply.github.com>
2024-07-23 18:43:59 +0900

    Add products pagination

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,5 +1,6 @@
 <script setup>
 import {onMounted, ref} from "vue";
+import { TailwindPagination } from 'laravel-vue-pagination';
 
 const categories = ref({})
 const products = ref({})
@@ -12,10 +13,10 @@ const getCategories = async () => {
         .catch((error) => console.log(error))
 }
 
-const getProducts = async () => {
-    await axios.get('/api/products')
+const getProducts = async (page = 1) => {
+    await axios.get(`/api/products?page=${page}`)
         .then((response) => {
-            products.value = response.data.data
+            products.value = response.data
         })
         .catch((error) => console.log(error))
 }
@@ -27,16 +28,26 @@ onMounted(() => {
 </script>
 
 <template>
-    <h1>Index Page</h1>
+    <h1 class="text-3xl font-bold">Index Page</h1>
 
-    <h2>Categories</h2>
+    <h2 class="text-xl font-bold">Categories</h2>
     <ul v-for="category in categories" :key="category.id">
         {{ category.name }}
     </ul>
 
-    <h2>Products</h2>
-    <ul v-for="product in products" :key="product.id">
-        {{ product.name }}
-    </ul>
+    <h2 class="text-xl font-bold">Products</h2>
+    <div class="grid grid-cols-4 gap-4">
+        <div class="space-y-2" v-for="product in products.data" :key="product.id">
+            <a href="#">
+                <img src="http://placehold.it/300x400" :alt="product.name" />
+            </a>
+            <a class="text-slate-500 text-xl font-semibold hover:underline">
+                {{ product.name}}
+            </a>
+            <p>${{ product.price}}</p>
+            <p class="prose-slate">{{ product.description }}</p>
+        </div>
+    </div>
 
+    <TailwindPagination :data="products" @pagination-change-page="getProducts" class="mt-4" />
 </template>
